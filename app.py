38ed563b3960c4cd378c5a54694fd5f0c898212d
richyst <ricardo_1994@hotmail.com>
2018-05-03 00:30:25 -0500

    puerto

--- app.py
+++ app.py
@@ -12,4 +12,4 @@ def recibir():
     return json.dumps(Modelo.predict(request.json))
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=4000)
+    app.run(debug=True, host='0.0.0.0', port=8080)
